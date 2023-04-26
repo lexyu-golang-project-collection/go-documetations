e67825a11b6cf5465de6c221e5eddf4b66fbf3f0
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-04-26 23:11:26 +0800

    in progress

--- tutorial-getting-started-with-generics/main.go
+++ tutorial-getting-started-with-generics/main.go
@@ -15,6 +15,8 @@ func main() {
 
 	fmt.Printf("Non-Generic Sums: %v and %v\n", SumInts(ints), SumFloats(floats))
 
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats(ints), SumIntsOrFloats[string, float64](floats))
 }
 func SumInts(m map[string]int64) int64 {
 	var s int64
@@ -26,6 +28,15 @@ func SumInts(m map[string]int64) int64 {
 
 func SumFloats(m map[string]float64) float64 {
 	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// generic function
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
