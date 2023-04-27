8aef0754161e8a1e10d390d567b2a7c378b5c2d8
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-04-27 22:35:37 +0800

    generics tutorial finished, pre fuzzing

--- tutorial-getting-started-with-generics/main.go
+++ tutorial-getting-started-with-generics/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -16,8 +20,15 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n", SumInts(ints), SumFloats(floats))
 
 	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats(ints), SumIntsOrFloats[string, float64](floats))
+		SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameter inferred: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints), SumNumbers(floats))
 }
+
 func SumInts(m map[string]int64) int64 {
 	var s int64
 	for _, v := range m {
@@ -36,6 +47,14 @@ func SumFloats(m map[string]float64) float64 {
 
 // generic function
 func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
