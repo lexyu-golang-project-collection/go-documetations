5917f5141b4cc69c2c2ad144ea1aa8ee8b8c71fd
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-05-09 23:57:57 +0800

    finish add a fuzz test. pre: fix the invalid string err

--- tutorial-getting-started-with-fuzzing/reverse_test.go
+++ tutorial-getting-started-with-fuzzing/reverse_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 
 	"golang.org/x/example/stringutil"
 )
@@ -21,5 +22,23 @@ func TestReverse(t *testing.T) {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
+}
+
+// go test -fuzz=Fuzz / -fuzztime
+func FuzzReverse(f *testing.F) {
+	testcase := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcase {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := stringutil.Reverse(orig)
+		doubleRev := stringutil.Reverse(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 
 }
