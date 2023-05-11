b95ef312c905ea4820d8ccd7d640b579a9689421
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-05-11 23:35:06 +0800

    in progress: Introducing the net/http package

--- tutorial-writing-web-applications/wiki.go
+++ tutorial-writing-web-applications/wiki.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 type Page struct {
 	Title string
@@ -8,8 +13,14 @@ type Page struct {
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
-	p1.save()
+	// p1 := &Page{Title: "test-page", Body: []byte("This is a sample page.")}
+	// p1.save()
+
+	// http.HandleFunc("/", handler)
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
@@ -24,8 +35,16 @@ func loadPage(title string) (*Page, error) {
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
-
 	}
-
 	return &Page{Title: title, Body: body}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:]) // The trailing [1:] means "create a sub-slice of Path from the 1st character to the end."
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
 }
